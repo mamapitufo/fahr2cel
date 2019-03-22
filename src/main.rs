1711b25a893adfce6f6c2c00fed67565b165115f
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-03-22 15:06:27 +0000

    Add fahr->celsius function and simple test code
    Initial version only handles integers.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let fahr = 32;
+    let celsius = fahrenheit_to_celsius(fahr);
+
+    println!("{} degrees Fahrenheit is {} degrees Celsius", fahr, celsius);
+}
+
+fn fahrenheit_to_celsius(deg: i32) -> i32 {
+    (deg - 32) * 5 / 9
 }
