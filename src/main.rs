e1acd061c16de7a5579d1ed8bced253994e80681
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-03-22 15:17:00 +0000

    Read temperature interactively with basic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use std::io;
+
 fn main() {
-    let fahr = 32;
+    println!("Enter the temperature in Fahrenheit:");
+
+    let mut fahr = String::new();
+    io::stdin()
+        .read_line(&mut fahr)
+        .expect("Failed to read temperature! (x_x)");
+
+    let fahr: i32 = match fahr.trim().parse() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("You must enter a number!");
+            std::process::exit(1);
+        }
+    };
+
     let celsius = fahrenheit_to_celsius(fahr);
 
     println!("{} degrees Fahrenheit is {} degrees Celsius", fahr, celsius);
