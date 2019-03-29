b062503dcd95306a89517a1418a44e02fac96f9c
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-03-29 19:28:07 +0000

    Add basic recursive search for a Fibonacci number

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let n = 3;
+
+    println!("Fibonacci({}) = {}", n, fibo(n));
+}
+
+fn fibo(n: i64) -> i64 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibo(n - 1) + fibo(n - 2)
+    }
 }
