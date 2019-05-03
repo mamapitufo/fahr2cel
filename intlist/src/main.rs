33ae3a7caf693898fe2809f0190f830afd684b52
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-05-03 23:11:19 +0100

    Sort arguments just after parsing them
    The sorted list of numbers is then cloned to an immutable reference, I think,
    and that's what we use from then on.

--- intlist/src/main.rs
+++ intlist/src/main.rs
@@ -2,12 +2,15 @@ use std::env;
 use std::iter::Iterator;
 
 fn main() {
-    let numbers: Vec<i32> = env::args()
+    let mut args: Vec<i32> = env::args()
         .skip(1)
         .filter_map(|arg| arg.trim().parse().ok())
         .collect();
+    args.sort();
 
-    println!("Non-numbers are filtered out!");
+    let numbers = args.clone();
+
+    println!("Anything that is not an integer is silently discarded!\n");
     println!("For the list {:?}:", numbers);
     println!("the mean is: {}", mean(&numbers));
 }
