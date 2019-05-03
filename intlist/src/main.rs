55613d3c17db51e549c3590e6ee419652b2fdc08
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-05-03 19:59:47 +0100

    Implement the `mean` function

--- intlist/src/main.rs
+++ intlist/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::iter::Iterator;
 
 fn main() {
     let numbers: Vec<i32> = env::args()
@@ -6,5 +7,13 @@ fn main() {
         .filter_map(|arg| arg.trim().parse().ok())
         .collect();
 
-    println!("The numbers: {:?}", numbers);
+    println!("Non-numbers are filtered out!");
+    println!("For the list {:?}:", numbers);
+    println!("the mean is: {}", mean(&numbers));
+}
+
+fn mean(numbers: &[i32]) -> f64 {
+    let sum = numbers.iter().fold(0, |acc, n| acc + n);
+
+    f64::from(sum) / numbers.len() as f64
 }
