f2da8ca6ed63308471f628f0573f7d51966b5eb0
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-05-03 23:27:11 +0100

    First stab at a `median()` function
    Index handling is still confusing, I think.

--- intlist/src/main.rs
+++ intlist/src/main.rs
@@ -13,10 +13,22 @@ fn main() {
     println!("Anything that is not an integer is silently discarded!\n");
     println!("For the list {:?}:", numbers);
     println!("the mean is: {}", mean(&numbers));
+    println!("the median is: {}", median(&numbers));
 }
 
 fn mean(numbers: &[i32]) -> f64 {
     let sum: i32 = numbers.iter().sum();
 
     f64::from(sum) / numbers.len() as f64
+}
+
+fn median(sorted: &[i32]) -> f64 {
+    let count = sorted.len();
+    let mid = count / 2;
+
+    if count % 2 == 0 {
+        (sorted[mid - 1] + sorted[mid]) as f64 / 2.0
+    } else {
+        sorted[mid] as f64
+    }
 }
