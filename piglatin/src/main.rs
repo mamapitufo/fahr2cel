6c3921a9eba2f2535dbb2a032ad5053f803b8428
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-05-08 16:52:43 +0100

    piglatin: Add fn to split a word so we can later reverse the parts
    Attempted an iterator-based approach (using `take_while`), but I could not make
    it work.

--- piglatin/src/main.rs
+++ piglatin/src/main.rs
@@ -1,2 +1,33 @@
 fn main() {
+    // let word = String::from("aardvark");
+    // let word = String::from("bee");
+    let word = String::from("crocodile");
+
+    let (prefix, suffix) = split_word(&word);
+
+    println!("word: {}, prefix: {}, suffix: {}", word, prefix, suffix);
+}
+
+fn split_word(word: &String) -> (String, String) {
+    let mut prefix = String::new();
+    let mut suffix = String::new();
+    let mut is_prefix = true;
+
+    for c in word.chars() {
+        if is_prefix && is_vowel(&c) {
+            is_prefix = false;
+        }
+
+        if is_prefix {
+            prefix.push(c);
+        } else {
+            suffix.push(c);
+        }
+    }
+
+    (prefix, suffix)
+}
+
+fn is_vowel(c: &char) -> bool {
+    *c == 'a' || *c == 'e' || *c == 'i' || *c == 'o' || *c == 'u'
 }
