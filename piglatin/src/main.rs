0d9ce00a8060a115f349ad18a4f3ae60ce4569f6
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-05-08 17:03:38 +0100

    piglatin: Take pre/suffix and return the pig latin word

--- piglatin/src/main.rs
+++ piglatin/src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
-    // let word = String::from("aardvark");
+    let word = String::from("aardvark");
     // let word = String::from("bee");
-    let word = String::from("crocodile");
+    // let word = String::from("crocodile");
 
+    println!("{} => {}", word, to_pig_latin(&word));
+}
+
+fn to_pig_latin(word: &String) -> String {
     let (prefix, suffix) = split_word(&word);
 
-    println!("word: {}, prefix: {}, suffix: {}", word, prefix, suffix);
+    if prefix.len() == 0 {
+        suffix + "way"
+    } else {
+        suffix + &prefix + "ay"
+    }
 }
 
 fn split_word(word: &String) -> (String, String) {
@@ -13,6 +21,7 @@ fn split_word(word: &String) -> (String, String) {
     let mut suffix = String::new();
     let mut is_prefix = true;
 
+    // FIXME: this should also check if the first letter is 'h'
     for c in word.chars() {
         if is_prefix && is_vowel(&c) {
             is_prefix = false;
