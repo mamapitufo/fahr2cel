83a549b6ff1ba23888dc7ea85f466d1e4552afba
Alberto Brealey-Guzman <alberto@feltfragments.com>
2019-05-11 18:43:17 +0100

    employees: Add command loop with `quit` command and help screen

--- employees/src/main.rs
+++ employees/src/main.rs
@@ -1,2 +1,28 @@
+use std::io;
+
 fn main() {
+    println!("Welcome to our People Inventory System!\n");
+
+    loop {
+        println!("Please input your command (or type 'help')");
+        let mut cmd = String::new();
+        io::stdin()
+            .read_line(&mut cmd)
+            .expect("Failed to read input (x_x)");
+
+        let cmd = cmd.trim();
+
+        if cmd == "quit" {
+            std::process::exit(0);
+        } else {
+            help();
+        }
+    }
+}
+
+fn help() {
+    println!("  Available Commands:\n  ===================");
+    println!("  Add Sally to Engineering: Adds 'Sally' to the 'Engineering' department.");
+    println!("  List Engineering:         Lists all employees in Engineering.");
+    println!("  List:                     List all employees, grouped by department.");
 }
